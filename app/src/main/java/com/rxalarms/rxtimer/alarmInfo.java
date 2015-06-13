4456680f5a190b3fa4db7d65077028957a8699d4
Tracey Wilson <twilson1@my.westga.edu>
2015-06-13 16:20:21 -0400

    worked on the layout for the screen to add a medicine

--- app/src/main/java/com/rxalarms/rxtimer/alarmInfo.java
+++ app/src/main/java/com/rxalarms/rxtimer/alarmInfo.java
@@ -1,31 +1,23 @@
 package com.rxalarms.rxtimer;
 
-import android.app.ActionBar;
-import android.content.Intent;
-import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TabHost;
-
-import java.lang.reflect.Type;
-
-public class MainActivity extends ActionBarActivity {
 
 
+public class alarmInfo extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_alarm_info);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_alarm_info, menu);
         return true;
     }
 
@@ -43,7 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-
-
 }
