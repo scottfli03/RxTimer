d03e5fc64544c14a98d0da5d0a90f55b47fe6815
Tracey <twilson1@my.westga.edu>
2015-06-18 10:11:26 -0400

    Added functionality to save data entered for a new alarm.

--- app/src/main/java/com/rxalarms/rxtimer/MainActivity.java
+++ app/src/main/java/com/rxalarms/rxtimer/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
