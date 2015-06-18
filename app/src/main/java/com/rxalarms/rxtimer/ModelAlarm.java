780764306142f783e2afdf06283bb3f3b7ff49da
scottfli03 <scott.flischel@gmail.com>
2015-06-18 00:58:54 -0400

    Added accessors to class

--- app/src/main/java/com/rxalarms/rxtimer/ModelAlarm.java
+++ app/src/main/java/com/rxalarms/rxtimer/ModelAlarm.java
@@ -28,6 +28,19 @@ public class ModelAlarm {
         this.alarmHour = 24;
         this.alarmMinutes = 0;
         this.isEnabled = false;
+        this.id = 0;
+    }
+
+    public void setID(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the ModelAlarm's ID.
+     * @return The ID
+     */
+    public long getID() {
+        return this.id;
     }
 
     /**
@@ -39,11 +52,27 @@ public class ModelAlarm {
     }
 
     /**
+     * Gets the ModelAlarm's name
+     * @return The patient's name
+     */
+    public String getPatient() {
+        return this.patient;
+    }
+
+    /**
      * Sets the medicine's name.
      * @param medicine  The medicine's name
      */
     public void setMedicine(String medicine) {
         this.medicine = medicine;
+    }
+
+    /**
+     * Gets the ModelAlarm's medicine name
+     * @return The name of the medicine
+     */
+    public String getMedicine() {
+        return this.medicine;
     }
 
     /**
@@ -55,11 +84,27 @@ public class ModelAlarm {
     }
 
     /**
+     * Gets the ModelAlarm's edicine name
+     * @return The dosage
+     */
+    public String getDosage() {
+        return this.dosage;
+    }
+
+    /**
      * Sets the special instructions.
      * @param instructions  The special instructions
      */
     public void setInstructions(String instructions) {
         this.instructions = instructions;
+    }
+
+    /**
+     * Gets the ModelAlarm's medicine instructions
+     * @return The instructions
+     */
+    public String getInstructions() {
+        return this.instructions;
     }
 
     /**
@@ -74,6 +119,14 @@ public class ModelAlarm {
     }
 
     /**
+     * Gets the ModelAlarm's alarm hour
+     * @return The hour
+     */
+    public int getHours() {
+        return this.alarmHour;
+    }
+
+    /**
      * Sets the alarm's minutes 0-59.
      * @param alarmMinutes the alarms minutes
      */
@@ -85,11 +138,27 @@ public class ModelAlarm {
     }
 
     /**
+     * Gets the ModelAlarm's alarm minutes
+     * @return The minutes
+     */
+    public int alarmMinutes() {
+        return this.alarmMinutes;
+    }
+
+    /**
      * Set's if the alarm is enabled.
      * @param isEnabled is the alarm enabled
      */
     public void setEnabled(boolean isEnabled) {
         this.isEnabled = isEnabled;
+    }
+
+    /**
+     * Returns if the ModelAlarm is enabled.
+     * @return if the ModelAlarm is enabled.
+     */
+    public boolean getIsEnabled() {
+        return isEnabled;
     }
 
     /**
