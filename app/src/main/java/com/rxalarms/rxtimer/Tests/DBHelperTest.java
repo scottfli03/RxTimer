2f126c189e382d0e4de5bc9df4e295012d57dbb8
scottfli03 <scott.flischel@gmail.com>
2015-06-20 15:14:09 -0400

    Added tests for AlarmDBHelper.

---
+++ app/src/main/java/com/rxalarms/rxtimer/Tests/DBHelperTest.java
@@ -0,0 +1,97 @@
+package com.rxalarms.rxtimer.Tests;
+
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import com.rxalarms.rxtimer.AlarmDBHelper;
+import com.rxalarms.rxtimer.ModelAlarm;
+
+/**
+ * Created by scottfli03 on 6/19/15.
+ *
+ * Tests the AlarmDBHelper methods.
+ */
+public class DBHelperTest extends AndroidTestCase {
+    AlarmDBHelper helper;
+    /**
+     * Creates an AlarmDBHelper object which will be used throughout testing.
+     * @throws Exception
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "Test_");
+        this.helper = new AlarmDBHelper(context);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Creates a template for the ModelAlarm.
+     * @return  The ModelAlarm
+     */
+    private ModelAlarm getModel() {
+        ModelAlarm alarm = new ModelAlarm();
+        alarm.setPatient("Dude");
+        alarm.setMedicine("Lovin");
+        alarm.setDosage("1 Dose");
+        alarm.setInstructions("With 2 drinks");
+        alarm.setAlarmHour(12);
+        alarm.setAlarmMinutes(34);
+        alarm.setEnabled(true);
+        return alarm;
+    }
+
+    /**
+     * Tests the helpers createAlarm() method.
+     */
+    public void testCreateAlarm() {
+        ModelAlarm alarm = this.getModel();
+        long id = helper.createAlarm(alarm);
+        ModelAlarm newAlarm = helper.getAlarm(id);
+        assertEquals("Dude", newAlarm.getPatient());
+        assertEquals("Lovin", newAlarm.getMedicine());
+        assertEquals("1 Dose", newAlarm.getDosage());
+        assertEquals("With 2 drinks", newAlarm.getInstructions());
+        assertEquals(12, newAlarm.getHours());
+        assertEquals(34, newAlarm.getMinutes());
+        assertTrue(newAlarm.getIsEnabled());
+    }
+
+    /**
+     * Tests the helpers updateAlarm() method.
+     */
+    public void testUpdateAlarm() {
+        ModelAlarm alarm = this.getModel();
+        alarm.setPatient("Jerry");
+        alarm.setDosage("Tons");
+        alarm.setMedicine("Happiness");
+        alarm.setInstructions("As much as possible.");
+        alarm.setAlarmHour(11);
+        alarm.setAlarmMinutes(11);
+        alarm.setEnabled(false);
+        long id = helper.createAlarm(alarm);
+        ModelAlarm newAlarm = helper.getAlarm(id);
+        assertEquals("Jerry", newAlarm.getPatient());
+        assertEquals("Happiness", newAlarm.getMedicine());
+        assertEquals("Tons", newAlarm.getDosage());
+        assertEquals("As much as possible.", newAlarm.getInstructions());
+        assertEquals(11, newAlarm.getHours());
+        assertEquals(11, newAlarm.getMinutes());
+        assertEquals(false, newAlarm.getIsEnabled());
+    }
+
+    /**
+     * Tests the helpers deleteAlarm() method
+     */
+    public void testDeleteAlarm() {
+        ModelAlarm alarm = this.getModel();
+        long id = helper.createAlarm(alarm);
+        int row = helper.deleteAlarm(id);
+        assertTrue(row == 1);
+        ModelAlarm newAlarm = helper.getAlarm(id);
+        assertEquals(null, newAlarm);
+    }
+}
