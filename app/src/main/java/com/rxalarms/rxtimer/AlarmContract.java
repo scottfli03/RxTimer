1d4cc352f97a9b8ebe5d9031feb7d4f1886397b3
scottfli03 <scott.flischel@gmail.com>
2015-06-28 13:52:23 -0400

    Added repeat to AlarmContract.

--- app/src/main/java/com/rxalarms/rxtimer/AlarmContract.java
+++ app/src/main/java/com/rxalarms/rxtimer/AlarmContract.java
@@ -25,6 +25,7 @@ public final class AlarmContract {
         public final static String COLUMN_NAME_MINUTES = "minutes";
         public final static String COLUMN_NAME_ISENABLED = "isEnabled";
         public final static String COLUMN_NAME_START_DATE = "startDate";
+        public final static String COLUMN_NAME_REPEAT = "repeat";
         public final static String COLUMN_NAME_END_DATE = "endDate";
         public final static String COLUMN_NAME_RINGTONE = "ringtone";
     }
